f53732e1eda31767fd75ef1384adb797b5d27670
caicangcua <36462470+caicangcua@users.noreply.github.com>
2023-10-22 17:09:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const express = require('express')
+//const express = require('express')
 
-const app = express()
-const PORT = 4000
+//const app = express()
+//const PORT = 4000
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+//app.listen(PORT, () => {
+//  console.log(`API listening on PORT ${PORT} `)
+//})
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳 do cho chet')
-})
+//app.get('/', (req, res) => {
+//  res.send('Hey this is my API running 🥳 do cho chet')
+//})
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+//app.get('/about', (req, res) => {
+//  res.send('This is my about route..... ')
+//})
 
-// Export the Express API
-module.exports = app
+//// Export the Express API
+//module.exports = app
+
+const PORT = 4000;
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Hello World!");
+    response.end();
+});
+
+server.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT} `);
+});
