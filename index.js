c15990453918e143971828f0763403b4b67e0e94
caicangcua <36462470+caicangcua@users.noreply.github.com>
2023-10-25 14:31:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 
 const http = require("http"), https = require('https'), ftp = require('ftp'),
