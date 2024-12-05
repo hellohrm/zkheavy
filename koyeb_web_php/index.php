156f04b1cd663c6980eda4abdde226815b8ef02b
hellohrm <61720510+hellohrm@users.noreply.github.com>
2024-12-05 18:46:41 +0700

    Add files via upload

---
+++ koyeb_web_php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Example PHP page</title>
+  </head>
+  <body>
+    <?php
+      echo '<h1>Hello world!</h1>';
+      echo '<p>This page uses PHP version '
+          . phpversion()
+          . '.</php';
+    ?>
+  </body>
+</html>
